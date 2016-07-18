a92c4719e0a499962cf44d98927107336aa6d4aa
hp <hp@ubuntu.ubuntu-domain>
2016-07-18 13:54:51 +0530

    remed me changes and adding access token in session

--- README.md
+++ README.md
@@ -31,28 +31,41 @@
 
     $twitter = new Twitter( $config );
     $post_id = $twitter->postStatusesUpdateWithMedia($picture, $link, $message);
+
 ### Share on Linkedin
 
+    /* creating object of LINKEDIN Class */
     $linkedin = new LINKEDIN( $config );
 
+    /* var intialization */
+    $accessToken = "";
+
+    /* Checking if user is already authenticated by Linkedin and access token saved in session */
+    if(isset( $_SESSION['accessToken'] ))
+        $accessToken = $_SESSION['accessToken'];
+
+    /* Checking code retrun from Linked in as callback to get access token */
     if (isset($_GET['code'])) {
         $code        = $_GET['code'];
+        /* Getting access token */
         $accessToken = $this->linkedin->getAccessToken($code);
+        /* Storing access token in session for next time sharing */
+        $_SESSION['accessToken'] = $accessToken;
     } else if (isset($_GET['error'])) {
         echo $_GET['error'];
     }
 
+    /* If user is not authenticated by Linkedin then send on Linkedin for user authentication */
     if (!$accessToken) {
         $this->linkedin->login();
         exit;
     } else {
-        $message = "";
-        $link    = "";
-        $picture = "";
+        $message = ""; // Message to share
+        $link    = ""; // Link to share
+        $picture = ""; // Picture url to share        
+        /*Sharing post*/
         $post_id = $this->linkedin->sharePost("", "", $message, $link, $picture);
     }
-
-
 ### Dependecies
         PHP 5.5
 
--- facebook_share.php
+++ facebook_share.php
@@ -2,24 +2,36 @@
 
     include_once('include/config.php');
     require_once('class/facebook.php');
-
+    /* Setting config array */
     $config['appId']     = FACEBOOK_APP_ID;
     $config['appSecret'] = FACEBOOK_APP_SECRET;
     $config['returnUrl'] = FACEBOOK_RETURN_URL;
 
+    /* creating object of Facebook Class */
 	$facebook    = new Facebook( $config ); 
 
+    /* var intialization */
     $accessToken = "";
-    if($accessToken == "" )
-        $accessToken = $this->facebook->getAccessToken();
 
+    /* Checking if user is already authenticated by Facebook and access token saved in session */
+    if(isset($_SESSION['accessToken'])){
+        $accessToken = $_SESSION['accessToken'];
+    }
+    /* Getting access token if it returns from facebook as callback */
+    if($accessToken == "" ){
+        /* Getting access token */
+        $accessToken = $this->facebook->getAccessToken();
+        /* Storing access token in session for next time sharing */
+        $_SESSION['accessToken'] = $accessToken;
+    }
+    
     if ($accessToken) {
-        $message              = "What to share"; // message to share
-        $link                 = "link to be share"; // sharing link
-        $picture              = "picture url to be share";	// picture url to be share
+        $message              = "What to share"; // Message to share
+        $link                 = "link to be share"; // Link to share
+        $picture              = "picture url to be share";	// Picture url to share
+        /*Sharing post on facebook */
         $post_id = $this->facebook->sharePost( $accessToken, $link, $message, $picture );
     } else {
+        /* Sending user on facebook for authentication */
         redirect($this->facebook->loginUrl());
     }
-    // end code to push listing on facebook //     
-?>
